047d22c4abdce6b68774a04b7ec3d076bffe5b1a
Joni Toyryla <info@jonitoyryla.eu>
2023-10-04 09:43:26 +0300

    Osa 1
    Tehtävä 4

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -1,18 +1,20 @@
 const App = () => {
 
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   const Header = (props) => {
     return <h1>{props.course}</h1>
@@ -24,8 +26,8 @@ const App = () => {
   
   const Content = (props) => {
     return ( 
-      props.parts.map(part => 
-      <Part part={part} />
+      props.parts.map((part,index) => 
+      <Part key={index} part={part} />
       // <Part name={part.name} exercises={part.exercises} />
     ))
   }
@@ -37,8 +39,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total total={parts.reduce((sum,part) => {return sum + part.exercises},0)} />
     </div>
   )
 }

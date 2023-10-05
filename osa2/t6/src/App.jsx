8dc031baa52ea3a40f0630bfb6d8cc2290d9a898
Joni Toyryla <info@jonitoyryla.eu>
2023-10-05 14:13:36 +0300

    Osa 2
    Tehtävä 6

---
+++ osa2/t6/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const Content = ({people}) => {
+  return people.map((person) => { 
+     // EI TOIMI (?): return <tr key={person.name + Date.now()}><td>{person.name}</td></tr> 
+     return <tr key={person.name + Math.random()}><td>{person.name}</td></tr> 
+  })
+}
+
+const App = () => {
+  const [people, setPeople] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('Gimme your name')
+
+  const addName = (event) => {
+    event.preventDefault();
+    console.log("addName()", newName)
+    setPeople(people.concat({name:newName}))
+  }
+
+  const inputHandler = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={inputHandler}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <table>
+        <tbody>
+          <Content people={people} />
+        </tbody>
+      </table>
+    </div>
+  )
+
+}
+
+export default App

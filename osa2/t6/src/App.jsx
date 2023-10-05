f2ed2ab467f6952cde81f1be8b7079bc0a67e731
Joni Toyryla <info@jonitoyryla.eu>
2023-10-05 14:48:47 +0300

    Osa 2
    Tehtävä 8

--- osa2/t6/src/App.jsx
+++ osa2/t6/src/App.jsx
@@ -3,45 +3,70 @@ import { useState } from 'react'
 const Content = ({people}) => {
   return people.map((person) => { 
      // EI TOIMI (?): return <tr key={person.name + Date.now()}><td>{person.name}</td></tr> 
-     return <tr key={person.name + Math.random()}><td>{person.name}</td></tr> 
+     return <tr key={person.name + Math.random()}><td>{person.name}</td><td>{person.number}</td></tr>
   })
 }
 
 const App = () => {
   const [people, setPeople] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', 'number': 123 }
   ]) 
   const [newName, setNewName] = useState('Gimme your name')
+  const [newNumber, setNewNumber] = useState('...and phone number')
 
-  const verifyName = (name) => {
-    // console.log("VerifyName() name:", name,
-    //   people.some((person) => person.name === name)
-    // )
-    return people.some((person) => person.name === name)
+  /*
+  Verify given values
+  ============================================
+  */
+  const hasName = () => {
+    return people.some((person) => person.name === newName)
+  }
+  const hasNumber = () => {
+    if (!newNumber) return false;
+    return true;
   }
 
-  const addName = (event) => {
+  /*
+  Add person.
+  ============================================
+  */
+  const addPerson = (event) => {
     event.preventDefault();
     // console.log("addName()", newName, verifyName(newName))
-    if (verifyName(newName)) { 
+    if (hasName()) {
       alert(`\"${newName}\" exists!`)
       return
     }
-    setPeople(people.concat({name:newName}))
+    if (!hasNumber()) {
+      console.log(newNumber)
+      alert("Missing number!")
+      return
+    }
+    setPeople(people.concat({name:newName, number:newNumber}))
   }
 
-  const inputHandler = (event) => {
-    // console.log(event.target.value)
+  /*
+  Input onChange handlers.
+  ============================================
+  */
+  const inputName = (event) => {
     setNewName(event.target.value)
+  }
+  const inputNumber = (event) => {
+    console.log("inputNumber() value:", event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={inputHandler}/>
-        </div>
+      <form onSubmit={addPerson}>
+        <table>
+          <tbody>
+          <tr><td>name: </td><td><input value={newName} onChange={inputName}/></td></tr>
+          <tr><td>number: </td><td><input value={newNumber} onChange={inputNumber}/></td></tr>
+          </tbody>
+        </table>
         <div>
           <button type="submit">add</button>
         </div>

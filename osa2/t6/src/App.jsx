90a6c8443e90f7d7cc18cf18a7ca86196b928524
Joni Toyryla <info@jonitoyryla.eu>
2023-10-06 11:25:38 +0300

    Osa 2
    Tehtävä 10

--- osa2/t6/src/App.jsx
+++ osa2/t6/src/App.jsx
@@ -16,13 +16,32 @@ function filter_stuff(arr, value) {
   return arr.filter((person) => person.name.match(regexp))
 }
 
+/* 
+  Components 
+  Filter
+  PersonForm
+  People (phonebook list)
+*/
 const Filter = ({onFilterChange}) =>
   <>Filter shown with <input onChange={onFilterChange}/></>
 
-/*
-  People component.
-  Returns a table of people.
+const PersonForm = ({onPersonSubmit,newName,newNumber,onNameChange,onNumberChange}) => {
+  return (
+  <form onSubmit={onPersonSubmit}>
+    <table>
+      <tbody>
+      <tr><td>name: </td><td><input value={newName} onChange={onNameChange}/></td></tr>
+      <tr><td>number: </td><td><input value={newNumber} onChange={onNumberChange}/></td></tr>
+      </tbody>
+    </table>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+  )
+}
 
+/*
   TODO: Unique key warning exists when concanate name and Date.now(). Random number works.
   => Ei toimi kun painaa nappia nopeasti: return <tr key={person.name + Date.now()}><td>{person.name}</td></tr> 
 */
@@ -37,7 +56,8 @@ const People = ({people}) => {
     </table>
   )
 }
- 
+
+/* Main component. */
 const App = () => {
   const [people, setPeople] = useState([
     { name: 'Arto Hellas', 'number': 123 },
@@ -63,11 +83,10 @@ const App = () => {
     return true;
   }
 
-  /*
-  submit handler
-  ============================================
+  /* Event Handlers
+  =====================================================
   */
-  const addPerson = (event) => {
+  const handlePersonSubmit = (event) => {
     event.preventDefault();
     console.log("App.addPerson() newName:", newName, "hasName():", hasName(newName))
     if (hasName()) {
@@ -93,16 +112,14 @@ const App = () => {
     setVisible(filter_stuff(result, newFilter))
   }
 
-  /*
-  Input onChange handlers.
-  ============================================
-  */
-  const inputName = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
-  const inputNumber = (event) => {
+
+  const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+
   const handleFilterChange = (event) => {
     // Show all people when filter is empty.
     if (!event.target.value) {
@@ -112,6 +129,9 @@ const App = () => {
     setNewFilter(event.target.value);  
     setVisible(filter_stuff(people, event.target.value))
   }
+  /*
+  =====================================================
+  */
 
   return (
     <div>
@@ -120,17 +140,13 @@ const App = () => {
       <Filter onFilterChange={handleFilterChange}/>
 
       <h2>Add new</h2>
-      <form onSubmit={addPerson}>
-        <table>
-          <tbody>
-          <tr><td>name: </td><td><input value={newName} onChange={inputName}/></td></tr>
-          <tr><td>number: </td><td><input value={newNumber} onChange={inputNumber}/></td></tr>
-          </tbody>
-        </table>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        onPersonSubmit={handlePersonSubmit}
+        onNameChange={handleNameChange}
+        onNumberChange={handleNumberChange}
+      />
 
       <h2>People</h2>
       <People people={visible} />
